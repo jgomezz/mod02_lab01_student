0b61f8412541926d9b9a95a1f80eb5029e3d9cce
jgomezz <jgomezz@gmail.com>
2025-03-30 08:26:13 -0500

    update

--- src/main/java/pe/edu/tecsup/app/Main.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.tecsup.app;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/pe/edu/tecsup/app/_01_ConsultarRegistrosJDBC.java
@@ -0,0 +1,68 @@
+package pe.edu.tecsup.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+//import static pe.edu.tecsup.app.Parametros.*;
+
+
+public class _01_ConsultarRegistrosJDBC {
+
+	// ENGINE : MYSQL  [jdbc:<<engine>>:<<port>>//<<IP o DOMINIO >>/<<SCHEMA>>?<<PARAMETROS OPCIONALES>> ]
+	static final String URL = "jdbc:mysql://localhost/almacen?useSSL=false";
+	static final String USERNAME = "root";
+	static final String PASSWORD = "";
+
+
+	/**
+	 *   
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		try {
+
+			// 1. Cargar el driver en memoria
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// 2. Conectarme a la BD
+			Connection con 
+				= DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			
+			// 3.Preparar la sentencia SQL
+			String sql = "SELECT id, nombre, descripcion FROM categorias";
+			
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			// 4. Obtener datos
+			ResultSet rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				
+				int id = rs.getInt("id");
+				
+				String nombre = rs.getString("nombre");
+
+				String descripcion = rs.getString("descripcion");
+
+				System.out.printf(">>> id = %d , nombre=%s, descripcion=%s \n", id, nombre, descripcion);
+				
+				//System.out.println(">>> id = " + id + ", nombre=" +nombre + ", descripcion="+ descripcion );
+			
+			}
+			
+			rs.close(); 
+			
+			stmt.close();
+			
+			con.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			//System.err.println(e.getMessage());
+		}
+	}
+
+}
